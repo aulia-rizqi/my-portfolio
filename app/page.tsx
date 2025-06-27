ccf337212b57b6f1ce70c57e034b2700a96b4300
auliarizqiqi <rizqiaulia216@gmail.com>
2025-06-27 19:12:08 +0700

    chore: remove unused import

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,6 @@ import AboutMe from "@/components/AboutMe"
 import Skills from "@/components/Skills"
 import Experience from "@/components/Experience"
 import Achievements from "@/components/Achievements"
-import Projects from "@/components/Projects"
-import Testimonials from "@/components/Testimonials"
 import Contact from "@/components/Contact"
 import Footer from "@/components/Footer"
 
